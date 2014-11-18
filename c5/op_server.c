9da36db93c1920de693cbdf0f8ff9df71579299a
hmg <tmdnldjazz@gmail.com>
2014-11-18 23:58:38 +0900

    add c10

--- c5/op_server.c
+++ c5/op_server.c
@@ -7,50 +7,54 @@
 
 #define BUF_SIZE 1024
 #define OPSZ 4
-void error_handling(char *message);
-int calculate(int opnum, int opnds[], char oprator);
 
-int main(int argc, char *argv[])
+int calculate(int opnum, int opnds[], char opator);
+
+int main(int argc, char* argv[])
 {
 	int serv_sock, clnt_sock;
 	char opinfo[BUF_SIZE];
 	int result, opnd_cnt, i;
-	int recv_cnt, recv_len;	
+	int recv_cnt, recv_len;
 	struct sockaddr_in serv_adr, clnt_adr;
 	socklen_t clnt_adr_sz;
-	if(argc!=2) {
+
+	if(argc != 2){
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
-	
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);   
-	if(serv_sock==-1)
-		error_handling("socket() error");
-	
+
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(serv_sock == -1)
+		printf("socket() error");
+
 	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family=AF_INET;
-	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serv_adr.sin_port=htons(atoi(argv[1]));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_adr.sin_port = htonl(atoi(argv[1]));
 
-	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
-		error_handling("bind() error");
-	if(listen(serv_sock, 5)==-1)
-		error_handling("listen() error");	
-	clnt_adr_sz=sizeof(clnt_adr);
+	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr)) == -1)
+		printf("bind() error");
 
-	for(i=0; i<5; i++)
+	if(listen(serv_sock, 5) == -1)
+		printf("listen() error");
+
+	clnt_adr_sz = sizeof(clnt_adr);
+
+	for(i = 0; i < 5; i++)
 	{
-		opnd_cnt=0;
-		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);	
+		opnd_cnt = 0;
+		clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_adr, &clnt_adr_sz);
+		//제일먼저 1바이트로 피연산자의 수를 받는다.
 		read(clnt_sock, &opnd_cnt, 1);
-		
-		recv_len=0;
-		while((opnd_cnt*OPSZ+1)>recv_len)
+
+		recv_len = 0;
+		while((opnd_cnt * OPSZ + 1) > recv_len)
 		{
-			recv_cnt=read(clnt_sock, &opinfo[recv_len], BUF_SIZE-1);
-			recv_len+=recv_cnt;
+			recv_cnt = read(clnt_sock, &opinfo[recv_len], BUF_SIZE-1);
+			recv_len += recv_cnt;
 		}
-		result=calculate(opnd_cnt, (int*)opinfo, opinfo[recv_len-1]);
+		result = calculate(opnd_cnt, (int*)opinfo, opinfo[recv_len-1]);
 		write(clnt_sock, (char*)&result, sizeof(result));
 		close(clnt_sock);
 	}
@@ -58,28 +62,26 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+
+
+
 int calculate(int opnum, int opnds[], char op)
 {
-	int result=opnds[0], i;
-	
+	int result = opnds[0], i;
 	switch(op)
 	{
 	case '+':
-		for(i=1; i<opnum; i++) result+=opnds[i];
+		for(i = 0; i < opnum; i++) result += opnds[i];
 		break;
+
 	case '-':
-		for(i=1; i<opnum; i++) result-=opnds[i];
+		for(i = 0; i < opnum; i++) result -= opnds[i];
 		break;
+
 	case '*':
-		for(i=1; i<opnum; i++) result*=opnds[i];
+		for(i = 0; i < opnum; i++) result *= opnds[i];
 		break;
 	}
 	return result;
 }
 
-void error_handling(char *message)
-{
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
-}
